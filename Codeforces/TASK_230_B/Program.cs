62f15d20c758e5158bf56ea3372451476223e8be
Artem Muradov <tym32167@gmail.com>
2019-05-17 00:08:02 +0200

    -

---
+++ Codeforces/TASK_230_B/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TASK_230_B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 1000000;
+            var numbers = new bool[n + 1];
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = true;
+
+            numbers[0] = false;
+            numbers[1] = false;
+
+            for (int i = 2; i <= n; ++i)
+            {
+                if (numbers[i])
+                {
+                    for (int j = i * 2; j <= n; j += i)
+                    {
+                        numbers[j] = false;
+                    }
+                }
+            }
+
+            var set = new HashSet<long>();
+
+            for (int i = 0; i < numbers.Length; i++)
+                if (numbers[i])
+                    set.Add(1L * i * i);
+
+            ReadOne();
+            var arr = ReadArray();
+            foreach (var a in arr)
+            {
+                if (set.Contains(a)) Console.WriteLine("YES");
+                else Console.WriteLine("NO");
+            }
+        }
+
+        public static long[] ReadArray()
+        {
+            return Console
+                .ReadLine()
+                .Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(long.Parse)
+                .ToArray();
+        }
+
+
+        public static int ReadOne()
+        {
+            return int.Parse(Console.ReadLine());
+        }
+    }
+}
---
+++ Codeforces/TASK_281_A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace TASK_281_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = Console.ReadLine();
+            var sb = new StringBuilder(line.Length);
+            sb.Append(char.ToUpper(line[0]));
+            for (var i = 1; i < line.Length; i++) sb.Append(line[i]);
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
